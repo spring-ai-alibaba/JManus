d02bd95dace717decba86ac79dc3cfc087486ca5
rainerWJY <answeropensource@alibabacloud.com>
2025-10-20 16:14:10 +0800

    build ui

--- src/main/resources/static/ui/assets/liquid-CzJOIyMF.js
+++ src/main/resources/static/ui/assets/liquid-CzJOIyMF.js
@@ -1,4 +1,4 @@
-import{m as l}from"./index-GRqJjwpW.js";import"./index-KH4bcIQe.js";import"./_plugin-vue_export-helper-52nTDeSC.js";import"./useMessage-CMbXBOUH.js";import"./LanguageSwitcher-CwH7mngi.js";/*!-----------------------------------------------------------------------------
+import{m as l}from"./index-MwdL9Eom.js";import"./index-BzpuArMA.js";import"./_plugin-vue_export-helper-CJZCuUvD.js";import"./useMessage-B5hIriOk.js";import"./LanguageSwitcher-DP-ttqwM.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.45.0(5e5af013f8d295555a7210df0d5f2cea0bf5dd56)
  * Released under the MIT license
